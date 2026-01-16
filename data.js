5b98fb749caccf7c824b4d139ecab642f90c3f4b
GitHub Action <action@github.com>
2026-01-16 07:28:49 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-01-16 14:40",
+  "update_time": "2026-01-16 15:28",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -16,43 +16,43 @@ window.SHU_DATA = {
       "tag": "forum"
     },
     {
-      "title": "传承大钟精神，逐梦智能时代：2025年度上海大学新闻传播学院“大钟奖学金”颁奖典礼顺利举行",
-      "url": "https://news.shu.edu.cn/info/1012/171405.htm",
+      "title": "百年人生映初心 校史添彩传薪火——房明毅先生回忆录捐赠仪式在上海大学顺利举行",
+      "url": "https://news.shu.edu.cn/info/1012/171475.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
     },
     {
-      "title": "2025第二届黄山国际传播论坛在上海大学成功举办",
-      "url": "https://news.shu.edu.cn/info/1012/171415.htm",
+      "title": "音乐与体育学部党建工作协调委员会召开2026年度第1次扩大会议",
+      "url": "https://news.shu.edu.cn/info/1012/171485.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
     },
     {
-      "title": "戏剧戏曲学科短片《台灵》荣获“电影中国”戏曲电影大展年度戏曲短片奖",
-      "url": "https://news.shu.edu.cn/info/1012/171425.htm",
+      "title": "加强社工人才培养 推动基层社会治理——上海经济管理中心为南汇新城镇举办2026年社工“育苗计划”培训班",
+      "url": "https://news.shu.edu.cn/info/1012/171495.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
     },
     {
-      "title": "上海大学一站式服务中心成立五周年总结与发展研讨会顺利召开",
-      "url": "https://news.shu.edu.cn/info/1012/171445.htm",
+      "title": "共商干部教育与专技教育发展新路径——上海经济管理中心召开第二十五次校务委员会暨国家级专业技术人员继续...",
+      "url": "https://news.shu.edu.cn/info/1012/171505.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
     },
     {
-      "title": "联合国、哥伦比亚大学代表团访问上海大学共商合作新篇",
-      "url": "https://news.shu.edu.cn/info/1012/171455.htm",
+      "title": "陈立群教授荣获2025年度教育部自然科学研究成果奖二等奖",
+      "url": "https://news.shu.edu.cn/info/1012/171525.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
     },
     {
-      "title": "外国语学院党委召开2025年党支部书记抓基层党建工作述职评议考核会",
-      "url": "https://news.shu.edu.cn/info/1012/171465.htm",
+      "title": "上海大学2025年妇女工作总结会暨2026年迎新活动顺利举行",
+      "url": "https://news.shu.edu.cn/info/1012/171535.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
