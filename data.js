189e6f6ccc697309ca22b2f4cab0ec0bf2a379ed
GitHub Action <action@github.com>
2026-01-19 13:52:08 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-01-19 20:59",
+  "update_time": "2026-01-19 21:52",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,20 +14,6 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
-    },
-    {
-      "title": "陈立群教授荣获2025年度教育部自然科学研究成果奖二等奖",
-      "url": "https://news.shu.edu.cn/info/1012/171525.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学2025年妇女工作总结会暨2026年迎新活动顺利举行",
-      "url": "https://news.shu.edu.cn/info/1012/171535.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
     },
     {
       "title": "上海市委统战部领导调研上海新的社会阶层研究中心等工作",
@@ -53,6 +39,20 @@ window.SHU_DATA = {
     {
       "title": "法国驻上海总领事馆科技领事Rachid MALTI一行访问理学院",
       "url": "https://news.shu.edu.cn/info/1012/171645.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海大学计算机学院校友分会第二届理事会第四次会议暨“海燕教育发展基金”第一届理事会第三次会议顺利召开",
+      "url": "https://news.shu.edu.cn/info/1012/171685.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "校地协同赋能智能计算新生态，上大计算机学院赴上海超级计算中心交流访问",
+      "url": "https://news.shu.edu.cn/info/1012/171695.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
