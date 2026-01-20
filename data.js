1e3a3830b512135494907cfec90bae16a96b7119
GitHub Action <action@github.com>
2026-01-20 08:38:49 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-01-20 15:32",
+  "update_time": "2026-01-20 16:38",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,27 +14,6 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
-    },
-    {
-      "title": "凝聚智慧力量，共绘发展蓝图——外国语学院党委召开党外人士、退休教师座谈会",
-      "url": "https://news.shu.edu.cn/info/1012/171595.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "我校召开干部教育培训工作领导小组第六次会议",
-      "url": "https://news.shu.edu.cn/info/1012/171635.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "法国驻上海总领事馆科技领事Rachid MALTI一行访问理学院",
-      "url": "https://news.shu.edu.cn/info/1012/171645.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
     },
     {
       "title": "计算机学院校友分会第二届理事会第四次会议暨“海燕教育发展基金”第一届理事会第三次会议顺利召开",
@@ -53,6 +32,27 @@ window.SHU_DATA = {
     {
       "title": "悉尼工商学院党委举行2025年度党支部书记抓基层党建述职评议考核会",
       "url": "https://news.shu.edu.cn/info/1012/171705.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "我校召开2026年精神文明建设工作会议",
+      "url": "https://news.shu.edu.cn/info/1012/171735.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海总工程师协会2025年会暨八届三次会员大会成功召开",
+      "url": "https://news.shu.edu.cn/info/1012/171725.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "2025年度机关党委党支部书记抓基层党建工作述职评议会顺利召开",
+      "url": "https://news.shu.edu.cn/info/1012/171745.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
