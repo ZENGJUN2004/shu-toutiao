5a1a819b6ffd73638445bb6fb66bc844c20c5ab8
GitHub Action <action@github.com>
2026-01-21 08:38:12 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-01-21 15:33",
+  "update_time": "2026-01-21 16:38",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -16,34 +16,6 @@ window.SHU_DATA = {
       "tag": "forum"
     },
     {
-      "title": "计算机学院校友分会第二届理事会第四次会议暨“海燕教育发展基金”第一届理事会第三次会议顺利召开",
-      "url": "https://news.shu.edu.cn/info/1012/171685.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "校地协同赋能智能计算新生态，上大计算机学院赴上海超级计算中心交流访问",
-      "url": "https://news.shu.edu.cn/info/1012/171695.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "悉尼工商学院党委举行2025年度党支部书记抓基层党建述职评议考核会",
-      "url": "https://news.shu.edu.cn/info/1012/171705.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "我校召开2026年精神文明建设工作会议",
-      "url": "https://news.shu.edu.cn/info/1012/171735.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
       "title": "上海总工程师协会2025年会暨八届三次会员大会成功召开",
       "url": "https://news.shu.edu.cn/info/1012/171725.htm",
       "source": "上大官网",
@@ -53,6 +25,34 @@ window.SHU_DATA = {
     {
       "title": "2025年度机关党委党支部书记抓基层党建工作述职评议会顺利召开",
       "url": "https://news.shu.edu.cn/info/1012/171745.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "倾听青年声音，共筑成长桥梁——外国语学院2025-2026学年师生座谈会顺利举行",
+      "url": "https://news.shu.edu.cn/info/1012/172005.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "凝心聚力守初心 双岗建功启新程——民盟上海大学委员会2025年度总结表彰大会顺利召开",
+      "url": "https://news.shu.edu.cn/info/1012/172015.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "悉尼工商学院召开2026年中层战略务虚会",
+      "url": "https://news.shu.edu.cn/info/1012/172025.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海航空与上海大学共促文化交流",
+      "url": "https://news.shu.edu.cn/info/1012/172035.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
