aa99f79a9bc2858bdde808960119e191259fd2f2
GitHub Action <action@github.com>
2026-01-27 05:32:09 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-01-27 12:10",
+  "update_time": "2026-01-27 13:32",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -58,17 +58,17 @@ window.SHU_DATA = {
       "tag": "official"
     },
     {
-      "title": "【中国科学报】永怀爱国之心 ——纪念“两弹一星”元勋郭永怀",
-      "url": "http://news.ustc.edu.cn/info/1056/74957.htm",
-      "source": "中国科学技术大学中国科大新闻网",
-      "time": "",
-      "tag": "media"
-    },
-    {
       "title": "新华时政_新华网",
       "url": "http://www.xinhuanet.com/politics/ggyslt/zbsl.htm",
       "source": "新华网",
       "time": "",
+      "tag": "media"
+    },
+    {
+      "title": "2025年社会学研究发展报告",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32463887",
+      "source": "澎湃新闻",
+      "time": "昨天11:42",
       "tag": "media"
     }
   ]
