28e8ba4c6dbd119a084be62516bebc0d4b3defa1
GitHub Action <action@github.com>
2026-02-02 17:41:59 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-02-03 00:48",
+  "update_time": "2026-02-03 01:41",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,6 +14,48 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
+    },
+    {
+      "title": "法学院党委领导班子召开2025年度民主生活会",
+      "url": "https://news.shu.edu.cn/info/1012/172615.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海市向明中学师生赴我校开展“筑梦上大，智创未来”研学活动",
+      "url": "https://news.shu.edu.cn/info/1012/172625.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "新闻传播学院召开2026年第一次全体教师大会暨2025年度先进工作者表彰大会",
+      "url": "https://news.shu.edu.cn/info/1012/172635.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "笃行不怠传薪火 赓续初心绘新卷——农工党上海大学支部委员会2025年度总结交流座谈会顺利召开",
+      "url": "https://news.shu.edu.cn/info/1012/172645.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "创意涌动赋能产业 产教融合共育新才——第六届全国会展策划创意大赛顺利落幕",
+      "url": "https://news.shu.edu.cn/info/1012/172705.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "情暖军营迎新春，军民同心谱新篇",
+      "url": "https://news.shu.edu.cn/info/1012/172695.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
     },
     {
       "title": "新华时政_新华网",
