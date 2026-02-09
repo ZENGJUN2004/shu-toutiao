a925f6628d873e2e1cd7fdd2f2e8ee68b4487467
GitHub Action <action@github.com>
2026-02-09 17:52:58 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-02-10 00:03",
+  "update_time": "2026-02-10 01:52",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,6 +14,48 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
+    },
+    {
+      "title": "情暖军营迎新春，军民同心谱新篇",
+      "url": "https://news.shu.edu.cn/info/1012/172695.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "学先进经验 促合作共赢——经管中心校友分会一行专访盐城校友企业",
+      "url": "https://news.shu.edu.cn/info/1012/172735.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "民革上大委员会开展春节走访慰问老党员活动",
+      "url": "https://news.shu.edu.cn/info/1012/172675.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海大学侨联新春送暖走访慰问老归侨侨眷",
+      "url": "https://news.shu.edu.cn/info/1012/172745.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "党建引领聚合力 校企协同促发展——上海大学一行赴上海软件中心调研交流",
+      "url": "https://news.shu.edu.cn/info/1012/172945.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "计算机学院举行2026年寒假留校学生慰问座谈会",
+      "url": "https://news.shu.edu.cn/info/1012/172955.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
     },
     {
       "title": "新华时政_新华网",
