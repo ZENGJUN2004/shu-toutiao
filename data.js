9c655423f4ba47a0335f5153072a0ab419522c6e
GitHub Action <action@github.com>
2026-02-21 08:37:00 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-02-21 15:35",
+  "update_time": "2026-02-21 16:37",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,13 +14,6 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
-    },
-    {
-      "title": "计算机学院举行2026年寒假留校学生慰问座谈会",
-      "url": "https://news.shu.edu.cn/info/1012/172955.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
     },
     {
       "title": "环化学院召开2026年国家自然科学基金和人才计划申报辅导会",
@@ -53,6 +46,13 @@ window.SHU_DATA = {
     {
       "title": "欧美同学会上大分会走访慰问80岁以上老学长",
       "url": "https://news.shu.edu.cn/info/1012/173055.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "面向国家战略，聚焦学科交叉，共话地理学新发展——上海市地理学会2025年学术年会暨理事会扩大会议在上海大...",
+      "url": "https://news.shu.edu.cn/info/1012/173075.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
