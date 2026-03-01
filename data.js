91c0a4ac0468bfcc2c34975fc650902df3652048
GitHub Action <action@github.com>
2026-03-01 06:58:07 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-03-01 13:57",
+  "update_time": "2026-03-01 14:58",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,62 +14,6 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
-    },
-    {
-      "title": "九三学社上海大学委员会开展新春慰问活动",
-      "url": "https://news.shu.edu.cn/info/1012/173015.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海市智能网联汽车网络安全重点实验室2025年度学术委员会会议顺利召开",
-      "url": "https://news.shu.edu.cn/info/1012/173025.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "马年迎春至，家访送暖情——上海大学开展2026年春节家访慰问活动",
-      "url": "https://news.shu.edu.cn/info/1012/173035.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "欧美同学会上大分会走访慰问80岁以上老学长",
-      "url": "https://news.shu.edu.cn/info/1012/173055.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "面向国家战略，聚焦学科交叉，共话地理学新发展——上海市地理学会2025年学术年会暨理事会扩大会议在上海大...",
-      "url": "https://news.shu.edu.cn/info/1012/173075.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学举办丙午马年国际学生新春沙龙",
-      "url": "https://news.shu.edu.cn/info/1012/173095.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "【科技日报】第二届智能科学家生态联盟大会在中国科学技术大学举办",
-      "url": "http://news.ustc.edu.cn/info/1056/89534.htm",
-      "source": "中国科学技术大学中国科大新闻网",
-      "time": "",
-      "tag": "media"
-    },
-    {
-      "title": "钱伟长逝世-新闻专题-科学网",
-      "url": "https://news.sciencenet.cn/news/sub2.aspx?id=388",
-      "source": "科学网",
-      "time": "",
-      "tag": "media"
     },
     {
       "title": "新华时政_新华网",
