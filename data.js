7111e01a138820389fda7e91ae7a72e8f811e1d4
GitHub Action <action@github.com>
2026-03-04 04:44:21 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-03-04 10:27",
+  "update_time": "2026-03-04 12:44",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,20 +14,6 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
-    },
-    {
-      "title": "马年迎春至，家访送暖情——上海大学开展2026年春节家访慰问活动",
-      "url": "https://news.shu.edu.cn/info/1012/173035.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "欧美同学会上大分会走访慰问80岁以上老学长",
-      "url": "https://news.shu.edu.cn/info/1012/173055.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
     },
     {
       "title": "面向国家战略，聚焦学科交叉，共话地理学新发展——上海市地理学会2025年学术年会暨理事会扩大会议在上海大...",
@@ -53,6 +39,20 @@ window.SHU_DATA = {
     {
       "title": "攻坚攀高峰，笃行开新局——机自学院2026年全院教职工大会顺利召开",
       "url": "https://news.shu.edu.cn/info/1012/173325.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "后勤保障部召开2026年工作推进会",
+      "url": "https://news.shu.edu.cn/info/1012/173425.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海大学计算机学院2个教授团队在上海市计算机学会2025年度教学成果奖评选中取得佳绩",
+      "url": "https://news.shu.edu.cn/info/1012/173435.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
