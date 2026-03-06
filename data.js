3aa65bea0606aee45fd24b38c880906aa37d0fa0
GitHub Action <action@github.com>
2026-03-06 04:45:17 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-03-06 10:29",
+  "update_time": "2026-03-06 12:45",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,20 +14,6 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
-    },
-    {
-      "title": "面向国家战略，聚焦学科交叉，共话地理学新发展——上海市地理学会2025年学术年会暨理事会扩大会议在上海大...",
-      "url": "https://news.shu.edu.cn/info/1012/173075.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学举办丙午马年国际学生新春沙龙",
-      "url": "https://news.shu.edu.cn/info/1012/173095.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
     },
     {
       "title": "悉尼工商学院召开新年教职工大会",
@@ -53,6 +39,20 @@ window.SHU_DATA = {
     {
       "title": "上海大学计算机学院2个教授团队在上海市计算机学会2025年度教学成果奖评选中取得佳绩",
       "url": "https://news.shu.edu.cn/info/1012/173435.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "微电子学院召开全院教职工大会暨2026年第3期教师政治理论学习",
+      "url": "https://news.shu.edu.cn/info/1012/173455.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海美术学院青年教师蔡丰盛荣膺2026年德国iF设计奖",
+      "url": "https://news.shu.edu.cn/info/1012/173465.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
