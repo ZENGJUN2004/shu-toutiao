2d216a2ef1c2be56ae307c0c132c5ec546b3adf6
GitHub Action <action@github.com>
2026-03-13 08:49:24 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-03-13 15:01",
+  "update_time": "2026-03-13 16:49",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,13 +14,6 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
-    },
-    {
-      "title": "国家重点研发计划“共生菌对发育和代谢的影响及干预”项目启动会在上海大学顺利召开",
-      "url": "https://news.shu.edu.cn/info/1012/173685.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
     },
     {
       "title": "巾帼不让须眉——致敬微电子学院“她风采”",
@@ -53,6 +46,13 @@ window.SHU_DATA = {
     {
       "title": "环化学院与上海市减污降碳管理运行技术中心开展人才联合培养交流会",
       "url": "https://news.shu.edu.cn/info/1012/173885.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "亲近自然，守护绿色：上海大学法学院联合后勤保障部开展“古银杏守护者”植树节主题活动",
+      "url": "https://news.shu.edu.cn/info/1012/173905.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
