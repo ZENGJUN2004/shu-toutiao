6fe57afa7c5777e3c44978f4372dbabc9e4dea5e
GitHub Action <action@github.com>
2026-03-18 07:07:19 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-03-18 13:00",
+  "update_time": "2026-03-18 15:07",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -16,43 +16,43 @@ window.SHU_DATA = {
       "tag": "forum"
     },
     {
-      "title": "环化学院与上海市减污降碳管理运行技术中心开展人才联合培养交流会",
-      "url": "https://news.shu.edu.cn/info/1012/173885.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "亲近自然，守护绿色：上海大学法学院联合后勤保障部开展“古银杏守护者”植树节主题活动",
-      "url": "https://news.shu.edu.cn/info/1012/173905.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学举办人工智能赋能教师教学能力提升系列研讨会（经管类专场）",
-      "url": "https://news.shu.edu.cn/info/1012/173935.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "钱伟长学院2025-2026学年专业分流宣讲活动——微电子学院专场顺利举行",
-      "url": "https://news.shu.edu.cn/info/1012/173955.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学侨联召开新学期工作会议 凝聚侨智侨力服务“十五五”开局",
-      "url": "https://news.shu.edu.cn/info/1012/173965.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
       "title": "党政办公室党支部与市政府办公厅政务服务处党支部召开专题联组学习会",
       "url": "https://news.shu.edu.cn/info/1012/173975.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "“金专计划”第四期开班仪式顺利举行",
+      "url": "https://news.shu.edu.cn/info/1012/174295.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海大学新闻传播学院党委部署开展树立和践行正确政绩观学习教育",
+      "url": "https://news.shu.edu.cn/info/1012/174305.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "锚定高质量发展 开启“十五五”新篇——上海大学新闻传播学院举行2026年第二次全体教师大会",
+      "url": "https://news.shu.edu.cn/info/1012/174315.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海电影学院教授携博士出版专著《中国画艺术十讲》获2026“中国好书”奖",
+      "url": "https://news.shu.edu.cn/info/1012/174325.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上电师生在多项AI创作大赛中斩获佳绩",
+      "url": "https://news.shu.edu.cn/info/1012/174335.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
