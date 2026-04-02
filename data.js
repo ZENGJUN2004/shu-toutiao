8a8a2dc61df34c3bb03015e5dcad935fc748815e
GitHub Action <action@github.com>
2026-04-02 07:19:53 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-04-02 13:07",
+  "update_time": "2026-04-02 15:19",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,27 +14,6 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
-    },
-    {
-      "title": "法国电影工作坊“艺术电影的海外策略”顺利结束",
-      "url": "https://news.shu.edu.cn/info/1012/174975.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学召开2026届毕业生就业工作推进会",
-      "url": "https://news.shu.edu.cn/info/1012/174985.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学新闻传播学院第二届青年教师联谊分会换届大会顺利举行",
-      "url": "https://news.shu.edu.cn/info/1012/174995.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
     },
     {
       "title": "“法治护航 聚力‘十五五’开局”——上海大学侨联联合市华侨事务中心举办侨界法治宣传专场活动",
@@ -58,6 +37,27 @@ window.SHU_DATA = {
       "tag": "official"
     },
     {
+      "title": "学校举行第四届纪委八次全体（扩大）会议",
+      "url": "https://news.shu.edu.cn/info/1012/175045.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "大型历史人文纪录片《大江南》 放映分享会走进上海电影学院",
+      "url": "https://news.shu.edu.cn/info/1012/175055.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "聚焦民生实事 共谋服务新篇——校工会妇委会与后勤保障部开展联组学习活动",
+      "url": "https://news.shu.edu.cn/info/1012/175065.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
       "title": "【科技日报】第二届智能科学家生态联盟大会在中国科学技术大学举办",
       "url": "http://news.ustc.edu.cn/info/1056/89534.htm",
       "source": "中国科学技术大学中国科大新闻网",
@@ -68,6 +68,13 @@ window.SHU_DATA = {
       "title": "新华时政_新华网",
       "url": "http://www.xinhuanet.com/politics/ggyslt/zbsl.htm",
       "source": "新华网",
+      "time": "",
+      "tag": "media"
+    },
+    {
+      "title": "历史上的今天|1922年3月18日,上海大学成立",
+      "url": "https://m.thepaper.cn/newsDetail_forward_32773330",
+      "source": "澎湃新闻",
       "time": "",
       "tag": "media"
     }
