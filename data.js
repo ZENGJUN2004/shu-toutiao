7d8ced9ebf7e82d9022a5523022515a647d99703
GitHub Action <action@github.com>
2026-04-10 02:55:05 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-04-10 07:40",
+  "update_time": "2026-04-10 10:55",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -16,34 +16,6 @@ window.SHU_DATA = {
       "tag": "forum"
     },
     {
-      "title": "泰国宋卡王子大学普吉孔子学院第十五届理事会顺利举行",
-      "url": "https://news.shu.edu.cn/info/1012/175135.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学青年教师联谊会外国语学院分会换届大会顺利举行",
-      "url": "https://news.shu.edu.cn/info/1012/175165.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "学两会精神 绘“十五五”蓝图——上海大学外国语学院召开全院教职工大会",
-      "url": "https://news.shu.edu.cn/info/1012/175175.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "共话改革 共谋发展：上海电影学院行政党支部与财务处党支部联合开展政绩观学习教育",
-      "url": "https://news.shu.edu.cn/info/1012/175195.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
       "title": "民进上大委员会3名会员参加民进上海市委宣传骨干培训班",
       "url": "https://news.shu.edu.cn/info/1012/175215.htm",
       "source": "上大官网",
@@ -53,6 +25,34 @@ window.SHU_DATA = {
     {
       "title": "上海大学召开上海新时代党的统一战线研究上海大学基地2026年工作推进会",
       "url": "https://news.shu.edu.cn/info/1012/175225.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "春启职途，智汇未来——上海大学2026年文科联盟就业招聘会顺利举办",
+      "url": "https://news.shu.edu.cn/info/1012/175275.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "教育部中外语言交流合作中心来我校调研交流",
+      "url": "https://news.shu.edu.cn/info/1012/175285.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "校党委第一巡察组巡察机关党委进驻动员会召开",
+      "url": "https://news.shu.edu.cn/info/1012/175295.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "体育学院开展内控专项监督检查与工作研讨",
+      "url": "https://news.shu.edu.cn/info/1012/175305.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
