5a8f50950ed4059fa993073420e7c1ae7dfcf48c
GitHub Action <action@github.com>
2026-04-12 15:40:16 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-04-12 22:43",
+  "update_time": "2026-04-12 23:40",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -60,6 +60,13 @@ window.SHU_DATA = {
     {
       "title": "【科技日报】第二届智能科学家生态联盟大会在中国科学技术大学举办",
       "url": "http://news.ustc.edu.cn/info/1056/89534.htm",
+      "source": "中国科学技术大学中国科大新闻网",
+      "time": "",
+      "tag": "media"
+    },
+    {
+      "title": "【中国科学报】永怀爱国之心 ——纪念“两弹一星”元勋郭永怀",
+      "url": "http://news.ustc.edu.cn/info/1056/74957.htm",
       "source": "中国科学技术大学中国科大新闻网",
       "time": "",
       "tag": "media"
