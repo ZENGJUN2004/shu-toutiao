ca88c8b02b19258c7cedac356e4c20ee0ac8b347
GitHub Action <action@github.com>
2026-04-20 03:24:43 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-04-20 07:41",
+  "update_time": "2026-04-20 11:24",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -16,34 +16,6 @@ window.SHU_DATA = {
       "tag": "forum"
     },
     {
-      "title": "重温民革党史·传承前辈精神——民革上海大学委员会组织党员活动",
-      "url": "https://news.shu.edu.cn/info/1012/175495.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上大民革党员参加上海民革新党员第二期“同心成长营”",
-      "url": "https://news.shu.edu.cn/info/1012/175575.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "我校两个案例入选教育部“长三角教育现代化”典型案例",
-      "url": "https://news.shu.edu.cn/info/1012/175585.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学2026年审计工作领导小组会议召开",
-      "url": "https://news.shu.edu.cn/info/1012/175595.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
       "title": "青春护航 五育融安——上海大学国家安全宣讲团2026年火热开讲",
       "url": "https://news.shu.edu.cn/info/1012/175615.htm",
       "source": "上大官网",
@@ -53,6 +25,34 @@ window.SHU_DATA = {
     {
       "title": "中国运筹学会期刊建设工作会议在上海大学召开",
       "url": "https://news.shu.edu.cn/info/1012/175485.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "企智合作赋能企业全球化发展 助力高校毕业生就业活动在上海大学举行",
+      "url": "https://news.shu.edu.cn/info/1012/175725.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "骑行追风 共启新程——上海大学行健社首次骑行活动圆满结束",
+      "url": "https://news.shu.edu.cn/info/1012/175735.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "第二届中国-葡语国家文明互鉴研讨会暨《巴西人：巴西的形成及意义》图书推介会顺利举行",
+      "url": "https://news.shu.edu.cn/info/1012/175745.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海智能计算系统工程技术研究中心2026年度技术委员会会议顺利召开",
+      "url": "https://news.shu.edu.cn/info/1012/175765.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
