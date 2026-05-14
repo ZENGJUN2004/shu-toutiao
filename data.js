c079e11ec95119de7cf333f11639e71f904c804d
GitHub Action <action@github.com>
2026-05-14 10:23:12 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-05-14 15:45",
+  "update_time": "2026-05-14 18:23",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -16,34 +16,6 @@ window.SHU_DATA = {
       "tag": "forum"
     },
     {
-      "title": "上海大学援疆教师饶进军获评“第十一批优秀援疆干部人才”并记功",
-      "url": "https://news.shu.edu.cn/info/1012/177115.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "国际心脏研究会主席Asa Gustafsson教授到访上海大学心血管研究所并作学术报告",
-      "url": "https://news.shu.edu.cn/info/1012/177125.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "国际心脏研究会东南亚分会主席 Roger Foo 教授到访上海大学心血管研究所并作学术报告",
-      "url": "https://news.shu.edu.cn/info/1012/177135.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "中国新闻传媒产教融合与教育创新研讨会在沪举行",
-      "url": "https://news.shu.edu.cn/info/1012/177145.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
       "title": "“国球宝山行，上大风景线” 上海大学代表队获上海乒乓嘉年华宝山区选拔赛亚军",
       "url": "https://news.shu.edu.cn/info/1012/177155.htm",
       "source": "上大官网",
@@ -53,6 +25,34 @@ window.SHU_DATA = {
     {
       "title": "上海大学召开2026年第二次党群工作例会",
       "url": "https://news.shu.edu.cn/info/1012/177225.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海市计算机学会2026年度学术年会在上海大学举行",
+      "url": "https://news.shu.edu.cn/info/1012/177265.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "2026年上海高校计算机学院院长论坛在上海大学举行",
+      "url": "https://news.shu.edu.cn/info/1012/177275.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海大学党委书记吴坚勇率团访问爱尔兰、瑞士",
+      "url": "https://news.shu.edu.cn/info/1012/177285.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "2026年怒江州高质量发展与现代化治理综合能力提升专题培训班启动",
+      "url": "https://news.shu.edu.cn/info/1012/177295.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
