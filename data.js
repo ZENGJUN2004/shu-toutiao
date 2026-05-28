6158c1ec8f5593c8f022439c4376923b92d483ba
GitHub Action <action@github.com>
2026-05-28 00:03:21 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-05-28 06:43",
+  "update_time": "2026-05-28 08:03",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,48 +14,6 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
-    },
-    {
-      "title": "上海大学机自学院学生获第十一届国际大学生智能农业装备创新大赛特等奖",
-      "url": "https://news.shu.edu.cn/info/1012/177905.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "法学院决策咨询专报撰写及国家社科基金申报专题交流会顺利举行",
-      "url": "https://news.shu.edu.cn/info/1012/177925.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "民进上海大学委员会顺利完成换届",
-      "url": "https://news.shu.edu.cn/info/1012/177945.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学荣获第十三届ASC世界大学生超算竞赛一等奖",
-      "url": "https://news.shu.edu.cn/info/1012/177965.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学科产系统工会开展春游团建活动",
-      "url": "https://news.shu.edu.cn/info/1012/177985.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "材料学院顺利举办2026年校友值年返校日活动",
-      "url": "https://news.shu.edu.cn/info/1012/178005.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
     },
     {
       "title": "【科技日报】第二届智能科学家生态联盟大会在中国科学技术大学举办",
