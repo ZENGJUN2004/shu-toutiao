7b5f56d8a0ab99123e647cfb14a4098e540ce45a
GitHub Action <action@github.com>
2026-06-02 21:51:15 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-06-03 02:43",
+  "update_time": "2026-06-03 05:51",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,6 +14,48 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
+    },
+    {
+      "title": "首届AI电影制作专业学员在上海大学上海温哥华电影学院结业，三个月交出这份“AI原生创作”答卷",
+      "url": "https://news.shu.edu.cn/info/1012/178465.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "理学院数学大师讲坛第二十、二十一讲顺利开讲",
+      "url": "https://news.shu.edu.cn/info/1012/178485.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "树立和践行正确政绩观！人文社科艺术处（智库管理办公室）开展“智库进学院”系列活动",
+      "url": "https://news.shu.edu.cn/info/1012/178495.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "法学院举行党委理论学习中心组（扩大）学习会暨班子集体研讨交流会",
+      "url": "https://news.shu.edu.cn/info/1012/178505.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "科产系统党委开展树立和践行正确政绩观学习辅导报告",
+      "url": "https://news.shu.edu.cn/info/1012/178515.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "环化学院与中海油能源发展有限公司上海安全环保分公司开展合作交流",
+      "url": "https://news.shu.edu.cn/info/1012/178525.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
     },
     {
       "title": "【科技日报】第二届智能科学家生态联盟大会在中国科学技术大学举办",
