fc2cd07d49824475e0141ca60ad732650b617d37
GitHub Action <action@github.com>
2026-06-09 07:54:40 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-06-09 11:55",
+  "update_time": "2026-06-09 15:54",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -16,34 +16,6 @@ window.SHU_DATA = {
       "tag": "forum"
     },
     {
-      "title": "监督贯通协同暨智慧监督专项工作推进会召开",
-      "url": "https://news.shu.edu.cn/info/1012/178975.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "羽动赛场展风采 凝心聚力促奋进——2026年上海大学教职工羽毛球团体赛顺利举办",
-      "url": "https://news.shu.edu.cn/info/1012/178985.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "新闻传播学院与医学院开展树立和践行正确政绩观学习教育联组学习",
-      "url": "https://news.shu.edu.cn/info/1012/179075.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "碳惠校园“账”未来  绿色转型“益”同行——沪碳行·上海大学第十八届环保节顺利落幕",
-      "url": "https://news.shu.edu.cn/info/1012/179085.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
       "title": "我校举行第三十九、四十期中青班暨二级党委青联会会长培训班开班式",
       "url": "https://news.shu.edu.cn/info/1012/179115.htm",
       "source": "上大官网",
@@ -53,6 +25,34 @@ window.SHU_DATA = {
     {
       "title": "上海大学理学院数学大师讲坛顺利开讲",
       "url": "https://news.shu.edu.cn/info/1012/179125.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "2026 年第六期“群雁齐飞计划”——上海院校“聚力协同共建，深耕双创育人”产教融合师资培训班在我校顺利举...",
+      "url": "https://news.shu.edu.cn/info/1012/179135.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海大学理学院数学学科发展研讨会顺利举行",
+      "url": "https://news.shu.edu.cn/info/1012/179145.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "湖南宜章访问团来校考察收集邓中夏同志建设上海大学生平事迹史料",
+      "url": "https://news.shu.edu.cn/info/1012/179155.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "学生档案之树常青——上海大学档案馆成功举办2026年国际档案日主题活动",
+      "url": "https://news.shu.edu.cn/info/1012/179165.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
