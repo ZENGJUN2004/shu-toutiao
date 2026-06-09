1e1cb29e2e87f877e2bbc98595a2f875eac7e461
GitHub Action <action@github.com>
2026-06-09 03:55:32 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-06-09 07:13",
+  "update_time": "2026-06-09 11:55",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -16,34 +16,6 @@ window.SHU_DATA = {
       "tag": "forum"
     },
     {
-      "title": "探秘微观生命 点亮科学童心——上海器官修复工程技术研究中心开展2026上海科技节开放日活动",
-      "url": "https://news.shu.edu.cn/info/1012/178925.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学9种期刊2025 CiteScore再创佳绩，国际影响力持续攀升",
-      "url": "https://news.shu.edu.cn/info/1012/178945.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "打通网络思政的“最后一公里”——我校举办辅导员网络思政专题学习",
-      "url": "https://news.shu.edu.cn/info/1012/178955.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "四届党委第四轮巡察整改部署会召开",
-      "url": "https://news.shu.edu.cn/info/1012/178965.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
       "title": "监督贯通协同暨智慧监督专项工作推进会召开",
       "url": "https://news.shu.edu.cn/info/1012/178975.htm",
       "source": "上大官网",
@@ -53,6 +25,34 @@ window.SHU_DATA = {
     {
       "title": "羽动赛场展风采 凝心聚力促奋进——2026年上海大学教职工羽毛球团体赛顺利举办",
       "url": "https://news.shu.edu.cn/info/1012/178985.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "新闻传播学院与医学院开展树立和践行正确政绩观学习教育联组学习",
+      "url": "https://news.shu.edu.cn/info/1012/179075.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "碳惠校园“账”未来  绿色转型“益”同行——沪碳行·上海大学第十八届环保节顺利落幕",
+      "url": "https://news.shu.edu.cn/info/1012/179085.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "我校举行第三十九、四十期中青班暨二级党委青联会会长培训班开班式",
+      "url": "https://news.shu.edu.cn/info/1012/179115.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海大学理学院数学大师讲坛顺利开讲",
+      "url": "https://news.shu.edu.cn/info/1012/179125.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
