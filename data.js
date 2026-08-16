cd71e6c53e933bbeb17dc7dd4306b3785d950825
GitHub Action <action@github.com>
2026-08-16 18:34:05 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-08-17 01:22",
+  "update_time": "2026-08-17 02:34",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -72,6 +72,13 @@ window.SHU_DATA = {
       "tag": "media"
     },
     {
+      "title": "【科技日报】第二届智能科学家生态联盟大会在中国科学技术大学举办",
+      "url": "https://news.ustc.edu.cn/info/1056/89534.htm",
+      "source": "中国科学技术大学中国科大新闻网",
+      "time": "",
+      "tag": "media"
+    },
+    {
       "title": "探索“科学家群体办刊”新模式 首批高质量学术期刊在沪创刊",
       "url": "https://www.stdaily.com/web/gdxw/2026-04/04/content_497889.html",
       "source": "中国科技网",
@@ -97,13 +104,6 @@ window.SHU_DATA = {
       "url": "https://www.thepaper.cn/newsDetail_forward_33187578",
       "source": "澎湃新闻",
       "time": "5月15日",
-      "tag": "media"
-    },
-    {
-      "title": "以青春之名,赴樱花之约!上海大学志愿者为2026上海樱花节注入温暖...",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32814875",
-      "source": "澎湃新闻",
-      "time": "3月23日",
       "tag": "media"
     },
     {
@@ -142,6 +142,13 @@ window.SHU_DATA = {
       "tag": "media"
     },
     {
+      "title": "上海大学钱文亮教授61岁突发疾病去世,兼任北大、上海师大研究员",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1154227",
+      "source": "上观",
+      "time": "8月2日",
+      "tag": "media"
+    },
+    {
       "title": "上海大学",
       "url": "https://www.thepaper.cn/tag/8688",
       "source": "澎湃新闻",
@@ -156,17 +163,10 @@ window.SHU_DATA = {
       "tag": "media"
     },
     {
-      "title": "上海大学悉尼工商学院SHU-UTS硕士项目举办国际创新大赛参赛项目...",
-      "url": "https://cn.chinadaily.com.cn/a/202605/26/WS6a155959a310942cc49ae644.html",
+      "title": "面向人人的科学教育——2026年\"全国科技活动周\"虹口区系列活动...",
+      "url": "https://caijing.chinadaily.com.cn/a/202606/01/WS6a1d2e45a310942cc49af519.html",
       "source": "中国日报网",
-      "time": "5月26日",
-      "tag": "media"
-    },
-    {
-      "title": "联合国青年办公室青年心理健康与幸福感计划启动在沪举行",
-      "url": "https://cn.chinadaily.com.cn/a/202606/15/WS6a2f9c0da310d709c2fb80f7.html",
-      "source": "中国日报网",
-      "time": "6月15日",
+      "time": "6月1日",
       "tag": "media"
     },
     {
@@ -177,10 +177,10 @@ window.SHU_DATA = {
       "tag": "media"
     },
     {
-      "title": "镜头定格临海新韵上海大学智能视听系千年府城采风",
-      "url": "https://sh.people.com.cn/n2/2026/0713/c134768-41637565.html",
-      "source": "人民网",
-      "time": "7月13日",
+      "title": "沪万教育协作再升级!首个沪万同步AI科创实验室正式启用",
+      "url": "https://caijing.chinadaily.com.cn/a/202606/25/WS6a3cdf71a310d709c2fba1b8.html",
+      "source": "中国日报网",
+      "time": "6月25日",
       "tag": "media"
     },
     {
