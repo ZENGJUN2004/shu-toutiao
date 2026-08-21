7b0658537804e2d55900d73834de539003de748b
GitHub Action <action@github.com>
2026-08-21 16:38:22 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-08-21 23:36",
+  "update_time": "2026-08-22 00:38",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -16,34 +16,6 @@ window.SHU_DATA = {
       "tag": "forum"
     },
     {
-      "title": "三十余国青年镜绘青浦，光影赋能文化出海！上大温影主办国际采风系列活动圆满收官",
-      "url": "https://news.shu.edu.cn/info/1012/182285.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学在第十三届全国高校物理实验教学研讨会自制仪器评比中斩获佳绩",
-      "url": "https://news.shu.edu.cn/info/1012/182295.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "课赛融合强赋能 聚力备赛育英才 ——环化学院召开本科生学科竞赛暨中国国际大学生创新大赛指导工作动员会",
-      "url": "https://news.shu.edu.cn/info/1012/182305.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "2026年TI杯上海市大学生电子设计竞赛测评在上海大学顺利举行",
-      "url": "https://news.shu.edu.cn/info/1012/182315.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
       "title": "“2026运动防治慢性病国际会议”在上海成功召开",
       "url": "https://news.shu.edu.cn/info/1012/182325.htm",
       "source": "上大官网",
@@ -53,6 +25,34 @@ window.SHU_DATA = {
     {
       "title": "上海大学法学院联合国家知识产权局审协天津中心举办“科研创新・专利实战营”",
       "url": "https://news.shu.edu.cn/info/1012/182335.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "理学院化学系学生在第十四届全国大学生化学实验竞赛总决赛中喜获佳绩",
+      "url": "https://news.shu.edu.cn/info/1012/182355.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "上海大学力工学院学子获第二届全国大学生智能建造竞赛一等奖",
+      "url": "https://news.shu.edu.cn/info/1012/182365.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "民进上海大学委员会6位教授被聘任为上海民进开明科普宣讲团专家",
+      "url": "https://news.shu.edu.cn/info/1012/182375.htm",
+      "source": "上大官网",
+      "time": "校内",
+      "tag": "official"
+    },
+    {
+      "title": "2026全国第十九届精密工程学术研讨会暨青年学者创新论坛成功举行",
+      "url": "https://news.shu.edu.cn/info/1012/182385.htm",
       "source": "上大官网",
       "time": "校内",
       "tag": "official"
