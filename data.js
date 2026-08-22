7dcee50758e5c704f3f9ae6cff51fa2a1b0a304d
GitHub Action <action@github.com>
2026-08-22 02:57:17 +0000

    Auto-update news

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.SHU_DATA = {
-  "update_time": "2026-08-22 09:24",
+  "update_time": "2026-08-22 10:57",
   "news": [
     {
       "title": "👉【B站】上海大学官方视频动态 (按发布排序)",
@@ -14,48 +14,6 @@ window.SHU_DATA = {
       "url": "https://s.weibo.com/weibo?q=上海大学&xsort=hot",
       "time": "实时",
       "tag": "forum"
-    },
-    {
-      "title": "“2026运动防治慢性病国际会议”在上海成功召开",
-      "url": "https://news.shu.edu.cn/info/1012/182325.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学法学院联合国家知识产权局审协天津中心举办“科研创新・专利实战营”",
-      "url": "https://news.shu.edu.cn/info/1012/182335.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "理学院化学系学生在第十四届全国大学生化学实验竞赛总决赛中喜获佳绩",
-      "url": "https://news.shu.edu.cn/info/1012/182355.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "上海大学力工学院学子获第二届全国大学生智能建造竞赛一等奖",
-      "url": "https://news.shu.edu.cn/info/1012/182365.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "民进上海大学委员会6位教授被聘任为上海民进开明科普宣讲团专家",
-      "url": "https://news.shu.edu.cn/info/1012/182375.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
-    },
-    {
-      "title": "2026全国第十九届精密工程学术研讨会暨青年学者创新论坛成功举行",
-      "url": "https://news.shu.edu.cn/info/1012/182385.htm",
-      "source": "上大官网",
-      "time": "校内",
-      "tag": "official"
     },
     {
       "title": "上海艺指委2025年度工作会议在上海大学举行—新闻—科学网",
